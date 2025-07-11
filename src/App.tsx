5c9c128c81d0af377a3c2a439d89bb549936b609
Franco Vargas <fragazmusic@gmail.com>
2025-07-11 16:50:58 -0300

    Deploy funcional com React corrigido

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      height: '100vh',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'sans-serif',
+      fontSize: '2rem'
+    }}>
+      FV Studios Dashboard está rodando!
+    </div>
+  )
+}
